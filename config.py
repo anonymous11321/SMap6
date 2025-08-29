cb32e99df9d13638a7ce658b43d8d21e0151056a
anonymous11321 <zhouyx6111@163.com>
2025-08-29 21:10:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# 新增 config.py 文件
+# 脨脗脭枚 config.py 脦脛录镁
 class Config:
     BUDGET = 50000000
-    INPUT_FILE = "Data/sampled_1M_addresses_0524.txt"
-    OUTPUT_TARGET = "Data/targets_0524.txt"
-    OUTPUT_ZMAP = "Data/output_0524.txt"
-    SOURCE_IP = "2408:4005:3af:8c00:1f2d:81b7:b902:7426"
-    SCAN_RATE = 1000
+    INPUT_FILE = "Data/seed_addresses.txt"
+    OUTPUT_TARGET = "Data/targets_addresses.txt"
+    OUTPUT_ZMAP = "Data/output.txt"
+    SOURCE_IP = "2408:4005:3af:8c00:1f2d:81b7:b902:7426" 
+    SCAN_RATE = 10000
