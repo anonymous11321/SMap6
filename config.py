11ad7b89cd9fe4c31faccede847b50017ae7d1c7
anonymous11321 <zhouyx6111@163.com>
2025-08-29 21:28:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     BUDGET = 50000000
-    INPUT_FILE = "Data/seed_addresses.txt"
-    OUTPUT_TARGET = "Data/targets_addresses.txt"
-    OUTPUT_ZMAP = "Data/output.txt"
+    INPUT_FILE = "./seed_addresses.txt"
+    OUTPUT_TARGET = "./targets_addresses.txt"
+    OUTPUT_ZMAP = "./output.txt"
     SOURCE_IP = "2408:4005:3af:8c00:1f2d:81b7:b902:7426" 
     SCAN_RATE = 10000
